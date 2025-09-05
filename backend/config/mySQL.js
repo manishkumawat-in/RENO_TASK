5e8544551e1f3d7609a43d29ca6786772448ca35
Manish Kumawat <manishkumawat1705@gmail.com>
2025-09-05 17:19:53 +0530

    Update mySQL.js

--- backend/config/mySQL.js
+++ backend/config/mySQL.js
@@ -7,6 +7,7 @@ try {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   });
 
   DBConnection.connect((err) => {
